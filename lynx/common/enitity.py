8e3dfcfe4dc17e84e5093b9ff86da453f984c5f7
FilipSzweda <filip.szweda1@gmail.com>
2023-03-06 22:13:11 +0100

    Change the abstract

--- lynx/common/enitity.py
+++ lynx/common/enitity.py
@@ -12,4 +12,17 @@ class Entity(Serializable):
         args: str = ""
 
     def export(self) -> Exported:
-        return self.Exported(type=type(self).__name__, args=self.serialize())
+        return self.Exported(type=type(self).__name__, args=super().serialize())
+
+    def serialize(self) -> str:
+        return self.export().serialize()
+
+    @classmethod
+    def deserialize(cls, json_string: str):  # TODO: returning `Self` was not working
+        from lynx.common.actions.move import Move
+        from lynx.common.object import Object
+
+        exported_entity = cls.Exported.deserialize(json_string)
+        entity_type = locals()[exported_entity.type]
+        entity = entity_type().populate(exported_entity.args)
+        return entity
--- lynx/common/scene.py
+++ lynx/common/scene.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Dict
 from lynx.common.object import *
-# TODO: we should make the imports less specific
-#       now, we must add each class here \/
-from lynx.common.actions.move import Move
 from lynx.common.serializable import Serializable
 from lynx.common.vector import Vector
 from lynx.common.enitity import Entity
@@ -11,29 +8,10 @@ from lynx.common.enitity import Entity
 
 @dataclass
 class Scene(Serializable):
-    # We are using separate class for serialization
-    @dataclass
-    class Exported(Serializable):
-        entities: List[Entity.Exported] = field(default_factory=list)
-
-    _entities: List[Entity] = field(default_factory=list)
-    objects_map: Dict[Vector, Object] = field(default_factory=dict)
-
-    def export(self) -> Exported:
-        exported_entities = [entity.export() for entity in self._entities]
-        return self.Exported(exported_entities)
-
-    def serialize(self) -> str:
-        return self.export().serialize()
-
-    def populate(self, json_string) -> None:
-        exported_scene = self.Exported.deserialize(json_string)
-        for exported_entity in exported_scene.entities:
-            entity_type = globals()[exported_entity.type]
-            entity = entity_type.deserialize(exported_entity.args)
-            self.add_entity(entity)
+    entities: List[Entity] = field(default_factory=list)
+    _objects_map: Dict[Vector, Object] = field(default_factory=dict)
 
     def add_entity(self, entity: Entity):
-        self._entities.append(entity)
+        self.entities.append(entity)
         if type(entity) is Object:
-            self.objects_map[entity.position] = entity
+            self._objects_map[entity.position] = entity
